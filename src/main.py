ab3512b5280ac557d9d8b191df9e840531edb8b7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-16 18:19:13 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -17,12 +17,6 @@ def main():
     logger: logging.Logger = logging.getLogger(__name__)
     logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
 
-    # Set up
-    setup: bool = src.preface.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
-    if not setup:
-        src.functions.cache.Cache().exc()
-        sys.exit('No Executions')
-
     # Data
     src.data.interface.Interface(service=service, s3_parameters=s3_parameters).exc()
 
@@ -70,6 +64,9 @@ if __name__ == '__main__':
     import src.analytics.interface
     import src.data.interface
 
+    import src.elements.s3_parameters as s3p
+    import src.elements.service as sr
+
     import src.functions.service
     import src.functions.cache
     import src.model.architecture
@@ -77,13 +74,13 @@ if __name__ == '__main__':
     import src.model.latest
     import src.model.tags
 
-    import src.s3.s3_parameters
-    import src.preface.setup
+    import src.preface.interface
     import src.transfer.interface
 
-    # S3 S3Parameters, Service Instance
-    connector = boto3.session.Session()
-    s3_parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
-    service = src.functions.service.Service(connector=connector, region_name=s3_parameters.region_name).exc()
+    connector: boto3.session.Session
+    s3_parameters: s3p
+    service: sr.Service
+    arguments: dict
+    connector, s3_parameters, service, arguments = src.preface.interface.Interface().exc()
 
     main()
