6fe1fdc9c3612dbb912f9cfcab8221b9f8f26ed0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-19 22:26:56 +0100

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -79,7 +79,6 @@ class Interface:
         # Get the artefacts metadata
         strings = src.data.artefacts.Artefacts(
             service=self.__service, s3_parameters=self.__s3_parameters).exc()
-        logging.info(strings)
 
         # The artefacts
         self.__data(strings=strings)
