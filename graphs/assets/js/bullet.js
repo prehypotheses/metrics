adf002d48b692979f35196a0815fa2f141192c99
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-18 21:48:24 +0100

    Update graphs/assets/js/bullet.js

--- graphs/assets/js/bullet.js
+++ graphs/assets/js/bullet.js
@@ -41,7 +41,7 @@ dropdown.on('change', function (e) {
 // Graphs/s
 function generateChart(fileNameKey) {
 
-    $.getJSON('../../warehouse/numerics/card/bullet/' + fileNameKey + '.json', function (calculations) {
+    $.getJSON('../../warehouse/metrics/card/bullet/' + fileNameKey + '.json', function (calculations) {
 
         const maximum = 0.45;
 
